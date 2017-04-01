41088a4fed544b63996a9da9e7a8793439ff0469
sivagarapati <siva.gsk333@gmail.com>
2017-03-31 23:35:31 -0500

    search app

---
+++ Search Box/Controller.js
@@ -0,0 +1,28 @@
+﻿var app = angular.module('myApp', []);
+
+
+
+app.controller("people", function($scope, $http) {
+    var self = this;
+    //$http.get("http://127.0.0.1/../data.json")
+    $http.get("http://localhost:3000/records")
+        .then(function(response) {
+            $scope.persons = response.data;
+        });
+});
+
+
+
+//app.service("dataBase", function ($http) {
+//    return {
+//        getData: function () {
+//            return $http.get("database.json",{responseType:"json"});
+//        }
+//    }
+//})
+
+//app.controller("peopleCtrl", function ($scope, dataBase) {
+//    dataBase.getData().success(function (records) {
+//        $scope.persons = records;
+//    })
+//})
