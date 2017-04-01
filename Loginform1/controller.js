cc1f6a1adc3183dbb94227b762549fcdce42a8d8
sivagarapati <siva.gsk333@gmail.com>
2017-03-31 21:08:35 -0500

    login app

--- Loginform1/controller.js
+++ Loginform1/controller.js
@@ -1,31 +1,31 @@
 ﻿var app = angular.module('mainApp', ['ngRoute']);
 
-app.config(function ($routeProvider) {
+app.config(function($routeProvider) {
     $routeProvider
-    .when('/', {
-        templateUrl:'login.html'
-    })
-    .when('/dashboard', {
-        //conditional routing
-        resolve: {
-            "check": function ($location,$rootScope) {
-                if (!$rootScope.loggedIn) {
-                    $location.path('/');
-                } 
-            }
-        },
-        templateUrl: 'dashboard.html'
-    })
-    .otherwise({
-        redirectTo:'/'
-    })
+        .when('/', {
+            templateUrl: 'login.html'
+        })
+        .when('/dashboard', {
+            //conditional routing
+            resolve: {
+                "check": function($location, $rootScope) {
+                    if (!$rootScope.loggedIn) {
+                        $location.path('/');
+                    }
+                }
+            },
+            templateUrl: 'dashboard.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
 });
 
-app.controller('loginCtrl', function ($scope,$location,$rootScope) {
-    $scope.submit = function () {
+app.controller('loginCtrl', function($scope, $location, $rootScope) {
+    $scope.submit = function() {
         //$rootScope attaches all of the objects to global objects
         //$rootScope.thisName = 'something';
-        
+
         //var uname = $scope.username;
         //var password = $scope.password;
         if ($scope.username == 'admin' && $scope.password == 'admin') {
@@ -34,7 +34,7 @@ app.controller('loginCtrl', function ($scope,$location,$rootScope) {
             //$rootScope.password = $scope.password;
             $rootScope.loggedIn = true;
             $location.path('/dashboard');
-        }else{
+        } else {
             alert('wrong stuff');
         }
     };
