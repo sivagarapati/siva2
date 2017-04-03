0c44f4ed4282d239fb60b583074620f6055993ab
sivagarapati <siva.gsk333@gmail.com>
2017-04-03 01:41:39 -0500

    provider

--- Provider/controller.js
+++ Provider/controller.js
@@ -1,10 +1,18 @@
 var app = angular.module("myApp", []);
 
 app.provider("date", function() {
+    var greet;
     return {
+        setGreeting: function(value) {
+            greet = value;
+        },
         $get: function() {
             return {
                 showDate: function() {
+                    var date = new Date();
+                    return greet + " Its " + date.getHours();
+                },
+                devshowDate: function() {
                     var date = new Date();
                     return date.getHours();
                 }
@@ -12,15 +20,21 @@ app.provider("date", function() {
         }
     }
 });
+app.config(function(dateProvider) {
+    var time = dataProvider.$get().devshowDate();
+    if (time > 0 && tme < 12) {
+        dataProvider.setGreeting("Good Morning!");
+    } else if (time > 12 && time < 17) {
+        dataProvider.setGreeting("Good day!");
+    } else if (time > 17 && time < 21) {
+        dateProvider.setGreeting("Good Evening!");
+    } else {
+        dateProvider.setGreeting("Good Night!");
+    }
+});
 //$route
-//$routeProvider
+//$routeProvider for dataProvider in config
 
 app.controller("providerCtrl", function($scope, date) {
-    $scope.Message = date.showDate();
+    $scope.greetMessage = date.showDate();
 });
-
-
-
-function providerCtrl() {
-
-}
