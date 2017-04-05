394ae20bbe7a467d5b9c47d7478daae355391dda
sivagarapati <siva.gsk333@gmail.com>
2017-04-05 17:29:22 -0500

    bookart

--- BooKart app/app.js
+++ BooKart app/app.js
@@ -1,21 +1,35 @@
 var app = angular.module("myApp", []);
 
-app.controller("headerCtrl", headerCtrl);
-
-function headerCtrl($scope) {
+app.controller("headerCtrl", function($scope) {
     $scope.appName = {
         title: "BookART",
         tagLine: "we have over 1 million Books"
-    }
+    };
+});
 
-}
 
-app.controller("booklistCtrl", booklistCtrl);
 
-function booklistCtrl($scope, $http) {
-    var self = this;
+app.config(function($routeProvider) {
+    $routeProvider.when("/books", {
+            templateUrl: "View/book-list.html",
+            controller: "booklistCtrl"
+        })
+        .when("/kart", {
+            templateUrl: "View/kart-list.html",
+            controller: "kartlistCtrl"
+        })
+        .otherwise({
+            redirectTo: "/books"
+        });
+});
+
+
+app.controller("booklistCtrl", function($scope, $http) {
+    //$http.get('/data/books.json').then(function(data) {
+    //  $scope.books = data;
     $http.get("http://localhost:3000/books")
         .then(function(response) {
             $scope.books = response.data;
         });
-}
+
+});
